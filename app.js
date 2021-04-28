55c66f798f1cc6dc7a2a6c9214d53b92f7d5d1b7
Rafael Vazquez <revazquez1990@github.com>
2021-04-28 00:00:59 -0400

    Add Menu

--- app.js
+++ app.js
@@ -2,33 +2,43 @@ const { Juego } = require('./class/Juego');
 const { Jugador } = require('./class/Jugador');
 const { Ronda } = require('./class/Ronda');
 
-let listaNombres;
-process.stdout.write('Favor ingrese los nombres de los jugadores separados por coma: \n');
-process.stdin.on('data', function(data){
-    listaNombres = data.toString().trim().split(',');
-    let codigo = 1;
-    const jugadores = listaNombres.reduce((arrJugadores,valorActual) => {
-        const jugador = new Jugador(valorActual,codigo);
-        arrJugadores.push(jugador);
-        codigo++;
-        return arrJugadores;
-    },[])
-    const juego = new Juego(jugadores);
-    desplegarMenu();
+let listaNombres, numData = 1, estado = 1;
+if (estado === 1) {
+    process.stdout.write('Favor ingrese los nombres de los jugadores separados por coma: \n');
+    process.stdin.on('data', function(data){
+    if (estado === 1) {
+        listaNombres = data.toString().trim().split(',');
+        console.log('entra aqui');
+        let codigo = 1;
+        const jugadores = listaNombres.reduce((arrJugadores,valorActual) => {
+            const jugador = new Jugador(valorActual,codigo);
+            arrJugadores.push(jugador);
+            codigo++;
+            return arrJugadores;
+        },[])
+        console.log(jugadores);
+        estado ++;
+        const juego = new Juego(jugadores);
+        desplegarMenu();
+    }
 });
+} 
 
 function desplegarMenu() {
     process.stdout.write('Marque la opcion deseada \n');
     process.stdout.write('Opcion 1: Jugar \n');
     process.stdout.write('Opcion 2: Salir \n');
     
-     process.stdin.once('readable', function(data){
-        if (data === 1) {
-            Ronda.asignarFichasAJugadores();
+    process.stdin.on('data', function(data){
+        if (data.toString().trim() === '1') {
+            console.log('entra aqui');
+            const ronda = new Ronda(numData, listaNombres);
+            console.log(ronda);
         }
     });
+}
 
-    const ronda1 = new Ronda(1, listaNombres);
-    console.log(ronda1);
+function crearFichas(params) {
+    
 }
 
